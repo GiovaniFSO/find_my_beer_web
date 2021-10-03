3a5a38cde6b4f10fcaa06fb19ec519cef82308ea
Giovani Fernandes <giovani_oli@hotmail.com>
2021-10-02 22:10:56 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useState, useEffect } from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 function App() {
+  const [latitude, setLatitude] = useState(0)
+  const [longitude, setLongitude] = useState(0)
+
+  const { REACT_APP_GOOGLE_API_KEY } = process.env;
+
+  useEffect(() =>{
+    setCurrentLocation()
+  }, [])
+
+  async function setCurrentLocation(){
+    await navigator.geolocation.getCurrentPosition(function (position){
+      setLatitude(position.coords.latitude)
+      setLongitude(position.coords.longitude)
+    }, function(error){
+      alert("Will trigger Dublin location to test the app :D")
+      setLatitude(53.3458948)
+      setLongitude(-6.2596063)
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <LoadScript googleMapsApiKey={REACT_APP_GOOGLE_API_KEY}>
+        <GoogleMap mapContainerStyle={{height: "100vh", width: "100%"}}
+          zoom={15} center={{lat: latitude, lng: longitude}}>
+
+        </GoogleMap>
+      </LoadScript>
+    </Fragment>
   );
 }
 
